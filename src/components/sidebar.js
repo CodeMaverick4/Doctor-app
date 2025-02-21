cd81f1f26365227d7d6b53c39f8dbca92986b52e
Burhan Ahmed <ahmed.burhan.ds@gmail.com>
2025-02-21 11:55:52 +0530

    change the sidebar icon to images from figma

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { SIDEBAR_CLIENT, SIDEBAR_AVAILABILITY, SIDEBAR_CALENDAR, SIDEBAR_DASHBOARD, SIDEBAR_LOGOUT, SIDEBAR_USER } from "../utils/images-constant.js"
+// import { SIDEBAR_CLIENT, SIDEBAR_AVAILABILITY, SIDEBAR_CALENDAR, SIDEBAR_DASHBOARD, SIDEBAR_LOGOUT, SIDEBAR_USER } from "../utils/images-constant.js"
+import {PatientIcon,AvailabilityIcon,CalendarIcon,DashboardIcon,DoctorIcon,LogoutIcon} from '../assets/all-sidebar-image-constant'
 import { Link, useLocation } from "react-router-dom";
 import { CalendarDays, LayoutGridIcon, LogOut, Repeat1, UserPen, UserPlus } from "lucide-react";
 
@@ -19,13 +20,13 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
 
   }, [])
   const menuItems = [
-    { id: 1, title: "Dashboard", link: '/', lucid_icon: <LayoutGridIcon size={30} />, icon: SIDEBAR_DASHBOARD },
-    { id: 2, title: "Patient Management", link: '/Patient', lucid_icon: <UserPen size={30} />, icon: SIDEBAR_USER, subItems: ["Upcoming", "Past", "Reschedule"] },
+    { id: 1, title: "Dashboard", link: '/', lucid_icon: <LayoutGridIcon size={30} />, icon: (color='#ffffff')=> <DashboardIcon color={color}/> },
+    { id: 2, title: "Patient Management", link: '/Patient', lucid_icon: <UserPen size={30} />, icon: (color='#ffffff')=> <PatientIcon color={color}/> , subItems: ["Upcoming", "Past", "Reschedule"] },
     
-    { id: 3, title: "Doctor Management", link: '/Doctor', lucid_icon: <UserPlus size={30} />, icon: SIDEBAR_CLIENT, subItems: ["Messages", "Alerts", "Updates"] },
-    { id: 4, title: "Availabilty Management", link: '/Availabilty', lucid_icon: <Repeat1 size={30} />, icon: SIDEBAR_AVAILABILITY, subItems: ["Reports", "Prescriptions", "Bills"] },
-    { id: 5, title: "Appointment Management", link: '/Appointment', lucid_icon: <CalendarDays size={30} />, icon: SIDEBAR_CALENDAR, subItems: ["Upcoming", "Past", "Reschedule"] },
-    { id: 6, title: "Logout", link: '', icon: SIDEBAR_LOGOUT, lucid_icon: <LogOut size={35} /> },
+    { id: 3, title: "Doctor Management", link: '/Doctor', lucid_icon: <UserPlus size={30} />, icon: (color='#ffffff')=> <DoctorIcon color={color}/> , subItems: ["Messages", "Alerts", "Updates"] },
+    { id: 4, title: "Availabilty Management", link: '/Availabilty', lucid_icon: <Repeat1 size={30} />, icon: (color='#ffffff')=> <AvailabilityIcon color={color}/> , subItems: ["Reports", "Prescriptions", "Bills"] },
+    { id: 5, title: "Appointment Management", link: '/Appointment', lucid_icon: <CalendarDays size={30} />, icon: (color='#ffffff')=> <CalendarIcon color={color}/> , subItems: ["Upcoming", "Past", "Reschedule"] },
+    { id: 6, title: "Logout", link: '', icon: (color='#ffffff')=> <LogoutIcon color={color}/> , lucid_icon: <LogOut size={35} /> },
     
   ];
   const toogleMenu = (e, title) => {
@@ -37,20 +38,20 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
     e.stopPropagation()
   }
   return (
-    <div onClick={(e) => e.stopPropagation()} className={`${showSidebar ? "" : "collapse"} vh-100 z-3 d-flex flex-column gap-3  align-items-center justify-content-center text-white transition-all sidebar `}>
+    <div onClick={(e) => e.stopPropagation()} className={`${showSidebar ? "" : "collapse"} vh-100 z-3 d-flex flex-column gap-2  align-items-center justify-content-center text-white transition-all sidebar `}>
       {/* <div className=" overflow-y-auto" style={{height:"90%"}} > */}
       {menuItems.map((item, index) => (
         <div key={index} className="cursor-pointer d-flex flex-column  justify-content-center position-relative">
           <div className={` text-center `} onClick={(e) => toogleMenu(e, item.title)}>
-            <span className={` ${location.pathname === item.link && "active"} sidebar-icon `}> {item.lucid_icon}</span>
+            <span className={` ${location.pathname === item.link && "active"} sidebar-icon `}> {item.icon(location.pathname === item.link ? "#0167FF": "#ffffff")}</span>
           </div>
-          <p className={`text-center ${location.pathname === item.link && "active"} sidebar-label mt-2`}>{item.title}</p>
+          <p className={`text-center ${location.pathname === item.link && "active"} sidebar-label mt-1`}>{item.title}</p>
           
-          {!item.subItems && (<Link to={item.link} className="stretched-link"></Link>)}
+          {!item.subItems && (<Link to={item.link} onClick={(e) => toogleMenu(e, item.title)} className="stretched-link "></Link>)}
 
           {item.subItems && (
             <div className={`sidebar-submenu ${showMenu === item.title ? "show" : ""}`}>
-              <Link to={item.link} className="text-text-decoration-none">
+              <Link to={item.link} className="text-decoration-none">
                 {item.subItems.map((subItem, subIndex) => (
                   <div key={subIndex} className="text-white px-3 py-2 sidebar-submenu-items">
                     {subItem}
